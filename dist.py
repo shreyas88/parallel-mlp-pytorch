0638e19dcf2f1e03b468d2ad5ed6d82341954743
Shreyas Srivastava <shreyas.srivastava@gmail.com>
2023-12-19 16:26:28 -0800

    fix error

--- dist.py
+++ dist.py
@@ -16,11 +16,11 @@ def my_test(weight_layer1,bias_layer1,weight_layer2,bias_layer2, x):
     weight_per_rank = torch.split(weight_layer1, output_size_per_partition, -1)[rank]
     bias_per_rank = torch.split(bias_layer1, output_size_per_partition, -1)[rank]
     myColParallelModule = ColumnParallelLinear(weight_per_rank, bias_per_rank).to(torch.cuda.current_device())
-    out, out_per_rank = myColParallelModule(x.to(torch.cuda.current_device()))
+    out_per_rank = myColParallelModule(x.to(torch.cuda.current_device()))
     #print("My rank",rank)
     #print(torch.cuda.get_device())
     print("Rank output:",out_per_rank.cpu().shape)
-    print("Output:",out.cpu().shape)
+    #print("Output:",out.cpu().shape)
 
 
 if __name__=='__main__':
