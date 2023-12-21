fix backprop issue

dist.py
@@ -17,12 +17,16 @@ def my_test(rank, queue, weight_layer1, bias_layer1, weight_layer2,bias_layer2, 
     rank = dist.get_rank()
     device_id = torch.cuda.current_device()
     output_size_per_partition = HIDDEN_DIM*2
-    weight_per_rank_layer1 = torch.split(weight_layer1, output_size_per_partition, -1)[rank].to(device_id)
-    bias_per_rank_layer1 = torch.split(bias_layer1, output_size_per_partition, -1)[rank].to(device_id)
+    weight_per_rank_layer1 = torch.split(weight_layer1, 
+                                         output_size_per_partition, -1)[rank].to(device_id)
+    bias_per_rank_layer1 = torch.split(bias_layer1, 
+                                       output_size_per_partition, -1)[rank].to(device_id)
     
-    weight_per_rank_layer2 = torch.split(weight_layer2, output_size_per_partition, 0)[rank].to(device_id)
+    weight_per_rank_layer2 = torch.split(weight_layer2, 
+                                         output_size_per_partition, 0)[rank].to(device_id)
 
-    myColParallelModule = ColumnParallelLinear(rank, weight_per_rank_layer1, bias_per_rank_layer1).to(device_id)
+    myColParallelModule = ColumnParallelLinear(rank, weight_per_rank_layer1, 
+                                               bias_per_rank_layer1).to(device_id)
     x_cuda = x.to(device_id)
     out_layer1_per_rank = myColParallelModule(x_cuda)
     